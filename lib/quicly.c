84e1aa72bcec2809ed60c3455e489d78bb2dbe4c
Hoang Tran <hoang.tran@uclouvain.be>
2019-10-22 11:48:02 +0200

    mp: recv: get path for each incoming packet
    store the found path index in the quicly_conn_t instead of passing
    around as an argument

--- lib/quicly.c
+++ lib/quicly.c
@@ -204,6 +204,7 @@ struct st_quicly_conn_t {
      *
      */
     struct {
+        uint8_t path;
         /**
          *
          */
@@ -4231,6 +4232,32 @@ static int is_stateless_reset(quicly_conn_t *conn, quicly_decoded_packet_t *deco
     return 1;
 }
 
+int quicly_lookup_path(quicly_conn_t *conn, struct sockaddr *dest_addr, struct sockaddr *src_addr,
+                          quicly_decoded_packet_t *decoded)
+{
+    quicly_conn_t *master_conn = conn->path->master_conn;
+    int i;
+    /* start from path 1, ignore path 0 */
+    for (i = 1; i < master_conn->num_rcv_paths; ++i)
+        if (conn->super.ctx->cid_encryptor != NULL) {
+            if (quicly_cid_is_equal(&master_conn->rcv_path[i]->conn->super.host.src_cid, decoded->cid.dest.encrypted))
+                goto Found;
+            if (is_stateless_reset(conn, decoded))
+                goto Found_StatelessReset;
+        }
+    /* not found */
+    return 0;
+
+Found:
+    conn->ingress.path = i;
+    decoded->_is_stateless_reset_cached = QUICLY__DECODED_PACKET_CACHED_NOT_STATELESS_RESET;
+    return 1;
+
+Found_StatelessReset:
+    decoded->_is_stateless_reset_cached = QUICLY__DECODED_PACKET_CACHED_IS_STATELESS_RESET;
+    return 1;
+}
+
 int quicly_is_destination(quicly_conn_t *conn, struct sockaddr *dest_addr, struct sockaddr *src_addr,
                           quicly_decoded_packet_t *decoded)
 {
@@ -4263,14 +4290,16 @@ int quicly_is_destination(quicly_conn_t *conn, struct sockaddr *dest_addr, struc
             return 0;
     }
 
-    /* not found */
-    return 0;
+    /* not found, check if it match any non-initial path */
+    return quicly_lookup_path(conn, dest_addr, src_addr, decoded);
 
 Found:
+    conn->ingress.path = quicly_lookup_path(conn, dest_addr, src_addr, decoded);
     decoded->_is_stateless_reset_cached = QUICLY__DECODED_PACKET_CACHED_NOT_STATELESS_RESET;
     return 1;
 
 Found_StatelessReset:
+    conn->ingress.path = 0; /* receive pkt on path 0 */
     decoded->_is_stateless_reset_cached = QUICLY__DECODED_PACKET_CACHED_IS_STATELESS_RESET;
     return 1;
 }
